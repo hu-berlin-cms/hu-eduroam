32293dcbe63bf70e3021ad15a51135a7d4779267
Enno Gröper <groepeen@cms.hu-berlin.de>
2014-11-08 13:31:52 +0100

    Indentation: tabs to spaces

--- app/src/main/java/nl/nikhef/eduroam/CSR.java
+++ app/src/main/java/nl/nikhef/eduroam/CSR.java
@@ -14,27 +14,28 @@ import org.bouncycastle.jce.PKCS10CertificationRequest;
 import org.bouncycastle.openssl.PEMWriter;
 
 public class CSR {
-	private static KeyPair pair;
-	private static String csr;
-	
-	public CSR(String username) throws Exception {
-		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-		KeyPairGenerator kpGen = KeyPairGenerator.getInstance("RSA", "BC");
-		kpGen.initialize(2048, new SecureRandom());
-		pair = kpGen.generateKeyPair();
-	    PKCS10CertificationRequest request = new PKCS10CertificationRequest("SHA256withRSA", new X500Principal(
-	    		"CN="+username), pair.getPublic(), null, pair.getPrivate());
-	    StringWriter stringWriter = new StringWriter();
-		PEMWriter pemWrt = new PEMWriter(stringWriter);
-		pemWrt.writeObject(request);
-		pemWrt.close();
-		csr = stringWriter.toString();
-	}
-	
-	public final PrivateKey getPrivate() {
-		return pair.getPrivate();
-	}
-	public final String getCSR() {
-		return csr;
-	}
+  private static KeyPair pair;
+  private static String csr;
+  
+  public CSR(String username) throws Exception {
+    Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+    KeyPairGenerator kpGen = KeyPairGenerator.getInstance("RSA", "BC");
+    kpGen.initialize(2048, new SecureRandom());
+    pair = kpGen.generateKeyPair();
+    PKCS10CertificationRequest request = new PKCS10CertificationRequest(
+        "SHA256withRSA", new X500Principal(
+          "CN="+username), pair.getPublic(), null, pair.getPrivate());
+    StringWriter stringWriter = new StringWriter();
+    PEMWriter pemWrt = new PEMWriter(stringWriter);
+    pemWrt.writeObject(request);
+    pemWrt.close();
+    csr = stringWriter.toString();
+  }
+  
+  public final PrivateKey getPrivate() {
+    return pair.getPrivate();
+  }
+  public final String getCSR() {
+    return csr;
+  }
 }
